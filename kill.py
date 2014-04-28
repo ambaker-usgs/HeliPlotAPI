09ab4c6d9c0a3d537e0d175dd4fb7289be579e5e
agonzales-usgs <agonzales@usgs.gov>
2014-04-28 15:35:03 -0600

    separated Heli station initialization, kill functions and cwbQuery functions to form a python API (need setup.py)

---
+++ kill.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+# -----------------------------------------------------------
+# Author: Alejandro Gonzales 
+# Filename: kill.py 
+# -----------------------------------------------------------
+# Purpose: Kill subprocess launches and/or multiprocessing
+#	   pool. These methods use parent/child/grandchild
+#	   pids to search and destroy hanging processes
+# -----------------------------------------------------------
+import psutil
+import signal
+import os, time
+
+class kill(object):
+	def killSubprocess(self, proc, childpid, signum):
+		# ---------------------------------------
+		# Kills pool subprocess child/grandchild
+		# proc.kill()
+		# proc.send_signal(signum)	# kill child (just in case)
+		# (out, err) = proc.communicate()
+		# raise subprocess.TimeoutExpired(proc.args, output=out)
+		# os.kill(proc.pid, signum)
+		# ---------------------------------------
+		print "Killing child:		%6s" % childpid
+		print "Killing grandchild:	%6s" % proc.pid
+		time.sleep(1)
+		# Send kill signal to child/grandchild (terminate/kill)
+		proc.terminate()	# stop grandchild proc
+		os.killpg(proc.pid, signum)	# kill grandchild process
+		os.killpg(childpid, signum)	# kill child process
+
+	def killPool(self, pool, pid, name):
+		# ---------------------------------------
+		# Kills multiprocessing pool and children
+		# sys.exit(0)
+		# os.wait()	# wait for all threads to exit
+		# ---------------------------------------
+		pool.terminate()
+		pool.join()
+		time.sleep(1)
+		print "Pool %s is terminated" % name
+		parent = psutil.Process(pid)
+		print "Parent pid:	%6s" % pid
+		print "Killling children of pool: %6s..." % pid
+		time.sleep(1)
+		# set recursive=True to kill grandchildren
+		for child in parent.get_children(recursive=True):
+			print child
+			child.kill()	# kill children of pool
+		print "Killing pool:	%6s..." % pid
+		time.sleep(1)
+		parent.kill()	# kill pool (parent)
---
+++ parallelcwbQuery.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# -----------------------------------------------------------
+# Author: Alejandro Gonzales 
+# Filename: parallelcwbQuery.py
+# -----------------------------------------------------------
+# Purpose: Creates multiprocessing pool to run multiple 
+#	   instances of CWBQuery.jar. CWBQuery pulls station
+#	   seed files from specified server (Golden/ASL)
+# -----------------------------------------------------------
+import multiprocessing
+from multiprocessing import Manager, Value
+import os, sys, string, subprocess
+import time
+
+# Necessary vars from main __init__ in HeliPlot.py
+'''
+self.seedpath
+self.stationinfo
+'''
+
+# Exception classes and cwbQuery() unpack function
+class KeyboardInterruptError(Exception): pass	
+class TimeoutExpiredError(Exception): pass	
+
+class parallelcwbQuery(object):
+	def launchWorkers(self):
+		# ---------------------------------------------
+		# Initialize all vars needed to run cwbQuery()
+		# ---------------------------------------------
+		print "------cwbQuery() Pool------\n"
+		self.home = os.getcwd()
+
